1e8c2d169810f714702fb57a791e4c98cc709d00
NatCode08 <natortizm@gmail.com>
2022-04-04 21:48:24 +0200

    added the open-source code

--- src/Weather.js
+++ src/Weather.js
@@ -8,9 +8,7 @@ export default function Weather() {
         <input type="search" placeholder="type a city" />
         <input type="submit" value="search" />
       </form>
-
       <h1>Scattered Clouds in Madrid</h1>
-
       <h3>Monday, April 4th 2022</h3>
       <p> 10 ºC | ºF </p>
       <div className="weatherDescription">
@@ -18,6 +16,16 @@ export default function Weather() {
         <br />
         Wind 3 km/h
       </div>
+      <br />
+      <br />
+      <a
+        href="https://github.com/NatCode08/weather-react-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>
+      <span> by Natalia Ortiz</span>
     </div>
   );
 }
