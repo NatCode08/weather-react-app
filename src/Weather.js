758e1fe64b97206c9ac1adac14233c372f94eb60
NatCode08 <natortizm@gmail.com>
2022-04-04 21:41:04 +0200

    Create a new weather app with a weather component and add some css changes

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div>
+      <form className="searchCity">
+        <input type="search" placeholder="type a city" />
+        <input type="submit" value="search" />
+      </form>
+
+      <h1>Scattered Clouds in Madrid</h1>
+
+      <h3>Monday, April 4th 2022</h3>
+      <p> 10 ºC | ºF </p>
+      <div className="weatherDescription">
+        Humidity 53%
+        <br />
+        Wind 3 km/h
+      </div>
+    </div>
+  );
+}
