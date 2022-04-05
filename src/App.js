a9d8cedbbeb1bf7330d8e4e2619d2cc512ad59ef
NatCode08 <natortizm@gmail.com>
2022-04-05 18:32:09 +0200

    update the footer link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
@@ -7,6 +7,17 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+
+      <footer>
+        This project was coded by Natalia Ortiz and is
+        <a
+          href="https://github.com/NatCode08/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source
+        </a>
+      </footer>
     </div>
   );
 }
